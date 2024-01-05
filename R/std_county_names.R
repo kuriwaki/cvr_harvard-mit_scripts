32fadd9c7d0ddca53ef2f00fd3e6697b664de060
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-01-05 12:26:32 -0500

    init repo

---
+++ R/std_county_names.R
@@ -0,0 +1,27 @@
+std_county_name <- function(cn) {
+  cn |>
+    str_replace("^ED(?= \\d)", "DISTRICT") |>
+    str_to_upper() |>
+    str_replace_all("_", " ") |>
+    str_replace("^SAINT", "ST.") |>
+    str_replace("^ST\\s", "ST. ") |>
+    # str_replace("JEFFERSON", "JEFF") |>
+    str_squish() |>
+    str_trim()
+}
+
+
+fix_county_name <- function(cn) {
+  cn |>
+    case_match(
+      "SAN BERARDINO" ~ "SAN BERNARDINO",
+      "PRINCE GEORGE" ~ "PRINCE GEORGE'S",
+      "TUSCAWARAS" ~ "TUSCARAWAS",
+      "CHAVEZ" ~ "CHAVES",
+      "EMMIT" ~ "EMMET",
+      "PALM" ~ "PALM BEACH",
+      "JOE DAVIESS" ~ "JO DAVIESS",
+      "BALTIMORE COUNTY" ~ "BALTIMORE",
+      .default = cn
+    )
+}
