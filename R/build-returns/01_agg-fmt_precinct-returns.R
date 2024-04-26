d6ae61c39c4fb10105d62cc5535b69fd6aa3291a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-04-26 17:37:29 -0400

    Script to load and format MEDSL returns file

---
+++ R/build-returns/01_agg-fmt_precinct-returns.R
@@ -0,0 +1,113 @@
+# Clean validation dataset from MEDSL to our format
+library(dataverse)
+library(tidyverse)
+library(arrow)
+library(fs)
+
+
+username <- Sys.info()["user"]
+
+# other users should make a different clause
+if (username %in% c("shirokuriwaki", "sk2983")) {
+  path_source <- "~/Dropbox/precincts20.sql" # only works for shirokuriwaki
+  path_outdir <- "~/Dropbox/CVR_parquet"
+}
+
+
+# Read whole data -----
+# Downloaded from https://github.com/MEDSL/precinct_sql_databases
+# last line 9910028 - 63
+tictoc::tic()
+ret_all <- read_tsv(
+  file = path_source,
+  skip = 62,
+  n_max = 9909966,
+  col_names = c("precinct", "office", "party_detailed", "party_simplified", "mode",
+                "votes", "county_name", "county_fips", "jurisdiction_name",
+                "jurisdiction_fips", "candidate", "district",
+                "dataverse", "year", "stage", "state", "special", "writein",
+                "state_po", "state_fips", "state_cen", "state_ic", "date",
+                "readme_check", "magnitude"),
+  col_types = "ccccciciciccciccllciiidli")
+tictoc::toc()
+
+statewide = c("ALASKA", "RHODE ISLAND")
+
+# only the top six offices -----
+## moist of data reformatting
+ret_sel <- ret_all |>
+  tidylog::filter(office %in% c("US PRESIDENT", "US HOUSE", "US SENATE",
+                                "STATE HOUSE", "STATE SENATE", "GOVERNOR")) |>
+  mutate(jurisdiction_name = replace(jurisdiction_fips, state %in% statewide, NA)) |>
+  mutate(jurisdiction_fips = replace(jurisdiction_name, state %in% statewide, NA),
+         county_name = replace(county_name, state %in% statewide, "STATEWIDE")) |>
+  arrange(state_fips, county_fips) |>
+  select(state,
+         matches("county"),
+         matches("jurisdiction"),
+         precinct,
+         mode,
+         office,
+         district,
+         magnitude,
+         special,
+         writein,
+         matches("party"),
+         candidate,
+         votes
+         ) |>
+  # to match to Harvard on party
+  mutate(writein = as.integer(writein),
+         special = as.integer(special),
+         party_detailed = case_when(candidate == "UNDERVOTES" ~ "UNDERVOTE",
+                                    candidate == "WRITEIN" ~ "WRITEIN",
+                                    candidate == "OVERVOTES" ~ "OVERVOTE",
+                                    .default = party_detailed)) |>
+  # match with Mason's definition of district
+  mutate(district = replace(district, office == "US PRESIDENT", "FEDERAL")) |>
+  mutate(dist_state = replace(state, !office %in% c("GOVERNOR", "US SENATE"), NA),
+         district = coalesce(dist_state, district),
+         district = str_pad(district, width = 3, pad = "0")) # ALASKA ST SEN needs padding
+
+
+# sum by county x mode
+county_mode_summ <- ret_sel |>
+  summarize(
+    votes = sum(votes, na.rm = TRUE),
+    .by = c("state", "county_name", "county_fips", "jurisdiction_name",
+            "jurisdiction_fips",  "office", "district",
+            "magnitude",  "special", "writein",
+            "mode",
+            "party_detailed", "party_simplified", "candidate",
+            )
+  )
+
+# sum by county
+county_summ <- county_mode_summ |>
+  summarize(
+    votes = sum(votes, na.rm = TRUE),
+    votes = sum(votes, na.rm = TRUE),
+    .by = c("state", "county_name", "county_fips", "jurisdiction_name",
+            "jurisdiction_fips",  "office", "district",
+            "magnitude",  "special", "writein",
+            "party_detailed", "party_simplified", "candidate",
+    )
+  )
+
+
+# write to parquet ---
+write_pq <- function(obj, pq_name, dir = path_outdir) {
+  obj |>
+    group_by(state, office) |>
+    write_dataset(
+      fs::path(dir, pq_name),
+      format = "parquet",
+      existing_data_behavior = "delete_matching"
+    )
+}
+
+write_pq(ret_sel,          "returns/by-precinct-mode")
+write_pq(county_summ,      "returns/by-county")
+write_pq(county_mode_summ, "returns/by-county-mode")
+
+
