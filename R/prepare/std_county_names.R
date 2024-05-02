87af9310f29d9118963f9d1575760b533b1daa2a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-05-02 19:15:45 -0400

    Delete old scripts

--- R/prepare/std_county_names.R
+++ R/prepare/std_county_names.R
@@ -9,31 +9,3 @@ std_county_name <- function(cn) {
     str_squish() |>
     str_trim()
 }
-
-
-fix_county_name <- function(cn) {
-  cn |>
-    case_match(
-      "SAN BERARDINO" ~ "SAN BERNARDINO",
-      "PRINCE GEORGE" ~ "PRINCE GEORGE'S",
-      "TUSCAWARAS" ~ "TUSCARAWAS",
-      "CHAVEZ" ~ "CHAVES",
-      "EMMIT" ~ "EMMET",
-      "PALM" ~ "PALM BEACH",
-      "JOE DAVIESS" ~ "JO DAVIESS",
-      "BALTIMORE COUNTY" ~ "BALTIMORE",
-      "BLECKLY" ~ "BLECKLEY",
-      "CHATOOGA" ~ "CHATTOOGA",
-      "EMMANUEL" ~ "EMANUEL",
-      "BLOOMINGTON" ~ "MCLEAN",
-      "GUADELUPE" ~ "GUADALUPE",
-      "GLASKCOCK" ~ "GLASCOCK",
-      "MIAMI DADE" ~ "MIAMI-DADE",
-      "OUTGAMIE" ~ "OUTAGAMIE",
-      "KING" ~ "KINGS",
-      "QUEEN ANNE" ~ "QUEEN ANNE'S",
-      "CHIPPEWAH" ~ "CHIPPEWA",
-      "EMMETT" ~ "EMMET",
-      .default = cn
-    )
-}
