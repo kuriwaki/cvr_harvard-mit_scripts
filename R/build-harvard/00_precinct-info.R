cb6b92e55b68f40ee4022a85f53596ea52fd75e1
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-05-21 08:48:05 -0400

    Add precinct info join from @JeffreyBLewis code

--- R/build-harvard/00_precinct-info.R
+++ R/build-harvard/00_precinct-info.R
@@ -2,25 +2,7 @@ library(tidyverse)
 library(haven)
 library(fs)
 
-# other users should make a different clause
-if (username %in% c("shirokuriwaki", "sk2983")) {
-  PATH_projdir <- "~/Dropbox/CVR_Data_Shared/data_main"
-  PATH_jbldir <- "~/Dropbox/CVR_Data_Shared/data_main/to-parquet/JBL/"
-  PATH_parquet <- "~/Downloads/CVR_parquet"
-}
-
-match_field_name <- function(values, pattern) {
-   str_detect(values,
-	     sprintf("(^|\\|)\\s*%s\\s*(\\||$)",
-	             pattern))
-}
-
-# See https://www.statology.org/r-add-column-if-does-not-exist/
-add_cols <- function(df, cols) {
-  add <- cols[!cols %in% names(df)]
-  if(length(add) != 0) df[add] <- NA_character_
-  return(df)
-}
+source("R/build-harvard/R/parse.R")
 
 # Paths ----
 filenames <- read_csv("R/build-harvard/input_files.txt", show_col_types = FALSE) |>
@@ -29,29 +11,31 @@ filenames <- read_csv("R/build-harvard/input_files.txt", show_col_types = FALSE)
 infonames <- filenames |>
   str_replace("_long\\.", "_cvr_info.") |>
   str_replace("STATA_long", "STATA_cvr_info")
+infonames <- infonames[file_exists(path(PATH_projdir, "STATA_cvr_info", infonames))]
 
 custom_cvr_info_field_mappings <- read_csv(
   path(PATH_jbldir, "csv/custom_cvr_info_field_mappings.csv"),
   show_col_types = FALSE)
 
 
-out <- map_dfr(
-  .x = infonames[1:10],
-  .f = function(x, dir = PATH_projdir) {
-    dat <- read_dta(fs::path(dir, "STATA_cvr_info", x), n_max = 10)
+tictoc::tic()
+cvr_info_meta <- map_dfr(
+  .x = infonames,
+  .f = function(.x, dir = PATH_projdir) {
 
     # get state and county
-    st <- parse_js_fname(x)["state"]
-    ct <- parse_js_fname(x)["county"]
+    st <- parse_js_fname(.x)["state"]
+    ct <- parse_js_fname(.x)["county"]
 
-    mutate(across(everything(), as.character)) |>
+    dat <- read_dta(fs::path(dir, "STATA_cvr_info", .x), n_max = 10) |>
+      mutate(across(everything(), as.character)) |>
       pivot_longer(
         everything(),
         names_to = "variable",
         values_to = "value") |>
       mutate(
-        state = state,
-        county = county,
+        state = st,
+        county = ct,
         .before = 1) |>
       summarize(
         values = paste(unique(value), collapse = "|"),
@@ -84,3 +68,53 @@ out <- map_dfr(
       mutate(new_name = coalesce(new_name.x, new_name.y)) |>
       select(-new_name.x, -new_name.y)
   })
+tictoc::toc()
+
+
+
+tictoc::tic()
+prec_info <- walk(
+  .x = infonames,
+  .f = function(.x, dir = PATH_projdir) {
+    cli::cli_alert_info("{.x}")
+    # get state and county
+    st <- parse_js_fname(.x)["state"]
+    ct <- parse_js_fname(.x)["county"]
+
+    # metdata
+    sel <- cvr_info_meta |>
+      filter(state == st,
+             county == ct,
+             !is.na(new_name),
+             new_name %in% c("precinct",
+                             "precinct_portion",
+                             "ballot_type")) |>
+      select(-state, -county, -values)
+
+    # read entire
+    haven::read_dta(fs::path(dir, "STATA_cvr_info", .x)) |>
+      filter(!is.na(cvr_id)) |>
+      mutate(across(everything(), as.character)) |>
+      pivot_longer(c(everything(), -cvr_id),
+                   names_to = "variable",
+                   values_to = "value") |>
+      right_join(sel, by = "variable", relationship = "many-to-one") |>
+      select(-variable) |>
+      summarize(value = paste0(value, collapse = "|"),
+                .by = c("cvr_id", "new_name")) |>
+      pivot_wider(id_cols = cvr_id,
+                  names_from = new_name,
+                  values_from = value) |>
+      add_cols("cvr_id_merged") |>
+      mutate(cvr_id_merged = as.integer(coalesce(cvr_id_merged, cvr_id))) |>
+      mutate(state = st, county = ct, .before = 1) |>
+      # Write to parquet
+      group_by(state, county) |>
+      arrow::write_dataset(
+        path = PATH_long_2,
+        format = "parquet",
+        existing_data_behavior = "delete_matching")
+  },
+  .progress = "counties"
+)
+tictoc::toc()
---
+++ R/build-harvard/R/parse.R
@@ -0,0 +1,33 @@
+
+# Output locations ------
+username <- Sys.info()["user"]
+
+# other users should make a different clause
+if (username %in% c("shirokuriwaki", "sk2983")) {
+  PATH_projdir <- "~/Dropbox/CVR_Data_Shared/data_main"
+  PATH_jbldir <- "~/Dropbox/CVR_Data_Shared/data_main/to-parquet/JBL/"
+  PATH_long_1 <- "~/Downloads/stata_init"
+  PATH_long_2 <- "~/Downloads/cvr_prec"
+}
+
+#' changes CA_Orange_long.dta to c("CA", "Orange")
+parse_js_fname <- function(chr) {
+  st <- str_sub(chr, 1, 2)
+  ct <- str_extract(chr, "(?<=[A-Z][A-Z]_).*(?=(_cvr_info|_long).dta)")
+  c(state = st, county = ct)
+}
+
+
+match_field_name <- function(values, pattern) {
+  str_detect(values,
+             sprintf("(^|\\|)\\s*%s\\s*(\\||$)",
+                     pattern))
+}
+
+# See https://www.statology.org/r-add-column-if-does-not-exist/
+add_cols <- function(df, cols) {
+  add <- cols[!cols %in% names(df)]
+  if(length(add) != 0) df[add] <- NA_character_
+  return(df)
+}
+
