668a0a24966bec5ad75ac8891ab8897fd3752e4a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-05-23 13:56:08 -0400

    Switch to duckplyr (hopefully faster?)

--- R/build-harvard/02_merge-party_snyder.R
+++ R/build-harvard/02_merge-party_snyder.R
@@ -1,24 +1,27 @@
 library(arrow)
+library(duckplyr)
 library(tidyverse)
 library(fs)
 
 source("R/build-harvard/R/parse.R")
 
 # Datasets -- metadata----
-meta <- open_dataset(
-  path(PATH_projdir, "to-parquet", "item_choice_info"))
-
-ds_orig <- open_dataset(PATH_long)
+meta    <- duckplyr_df_from_parquet(path(PATH_projdir, "to-parquet", "item_choice_info/*.parquet"))
+ds_orig <- duckplyr_df_from_parquet(PATH_long2) |>
+  select(-line_number)
+ds_prec <- duckplyr_df_from_parquet(path(PATH_prec, "*/*/*.parquet"))
 
 # Main merge ----
 
 # Have to do this state by state since my R crashes otherwise
 states_vec <- distinct(ds_orig, state) |>
-  pull(state, as_vector = TRUE) |>
+  pull(state) |>
   sort()
 
 tictoc::tic()
 for (st in states_vec) {
+
+  cli::cli_alert_info("{st}")
 
   ds <- ds_orig |>
     filter(state == st) |>
@@ -33,7 +36,7 @@ for (st in states_vec) {
       relationship = "many-to-one")
 
   # Precinct
-  ds_prec <- open_dataset(PATH_prec) |>
+  ds_prec_st <- ds_prec |>
     filter(state == st)
 
   # Party ID ---
@@ -45,6 +48,12 @@ for (st in states_vec) {
       R = as.numeric(party == "REP"),
     ) |>
     summarize(
+      pres = case_when(
+        any(party == "DEM" & item == "US_PRES") ~ "D",
+        any(party == "REP" & item == "US_PRES") ~ "R",
+        any(party == "LBT" & item == "US_PRES") ~ "LBT",
+        .default = NA_character_
+      ),
       D = sum(D),
       R = sum(R),
       .by = c(state, county, cvr_id)
@@ -60,19 +69,19 @@ for (st in states_vec) {
         pid_num == 0 ~ "SPL",
         pid_num == -1 ~ "REP"
       )) |>
-    select(state, county, cvr_id, pid, pid_num)
+    select(state, county, cvr_id, pres, pid, pid_num)
 
 
   # Write ----
   ds |>
     # merge prec
-    left_join(ds_prec, by = c("state", "county", "cvr_id")) |>
+    left_join(ds_prec_st, by = c("state", "county", "cvr_id")) |>
     # merge national party
     left_join(
       ds_pid,
       by = c("state", "county", "cvr_id"),
       relationship = "one-to-one") |>
-    relocate(pid, pid_num, .after = cvr_id) |>
+    relocate(pres, pid, pid_num, .after = cvr_id) |>
     group_by(state, county) |> # specifies partition
     write_dataset(
       path = PATH_merged,
