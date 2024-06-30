8662b2460103bb7885b232fee9f16af65ea6bfd7
Mason Reece <mpreece@mit.edu>
2024-06-30 00:00:37 -0400

    speedup standalone header processing script

--- R/build-medsl/code/process_headers.R
+++ R/build-medsl/code/process_headers.R
@@ -1,16 +1,20 @@
-library(tidyverse)
-library(glue)
+library(readr)
+library(stringr)
 
 # Get the command-line arguments
 file <- commandArgs(trailingOnly = TRUE)[1]
 file_new = str_replace(file, "\\.csv", "_headers.csv")
 
-df <- read_csv(file,
+df <- read_csv(
+  file,
   col_types = cols(.default = "c"),
-  n_max = 2,
   skip = 1,
   show_col_types = FALSE
 )
+
+bad_rows = which(df[20] == colnames(df)[20] |> str_remove("\\.\\.\\.\\d+")) |> 
+  purrr::map(~ (.x - 1):(.x + 2)) |> 
+  unlist()
 
 colnames(df) = paste(colnames(df), df[1, ], df[2, ], sep = "_") |> 
   str_remove("\\.\\.\\.\\d+") |> 
@@ -18,7 +22,5 @@ colnames(df) = paste(colnames(df), df[1, ], df[2, ], sep = "_") |>
   janitor::make_clean_names(case = "title") |> 
   iconv(to = "UTF-8", sub = "")
 
-df <- df[-1:-2, ]
-
-write_csv(df, file_new)
-system(glue("tail -n +5 {file} >> {file_new}"))
+df[-c(bad_rows, 1, 2), ] |> 
+  write_csv(file_new)
