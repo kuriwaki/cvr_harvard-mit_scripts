446ec46d92ad4d667c65170af9e667d685395596
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-07-01 14:23:34 -0400

    na.rm unnecessary because I replaced NAs with 0

--- R/combine/01c_classification-R.R
+++ R/combine/01c_classification-R.R
@@ -5,7 +5,8 @@ categorize_diff <- function(tbl, var, newvar, candvar) {
     filter(!is.na({{candvar}}) | !is.na(candidate_v)) |>
     mutate(
       var_missing = is.na({{var}}),
-      {{var}} := replace_na({{var}}, 0), # is missing when others are present, change to 0
+      # is missing when others are present, change to 0 (but still track which rows were NA by `var_missing`)
+      {{var}} := replace_na({{var}}, 0),
       diff  = votes_v - {{var}},
       diffm = votes_h - votes_m) |>
     summarize(
@@ -16,10 +17,10 @@ categorize_diff <- function(tbl, var, newvar, candvar) {
     summarize(
       {{newvar}} := case_when(
         all(diff == 0, na.rm = FALSE) ~ "0 difference",
-        all(!office_miss) & all(adiff < 0.01, na.rm = FALSE) & all(adiff <  0.01, na.rm = FALSE) & any(diff != 0, na.rm = FALSE) ~ "any < 1% mismatch",
-        all(!office_miss) & all(adiff < 0.05, na.rm = FALSE) & any(adiff >= 0.01, na.rm = FALSE) ~ "any < 5% mismatch",
-        all(!office_miss) & all(adiff < 0.10, na.rm = FALSE) & any(adiff >= 0.05, na.rm = FALSE) ~ "any < 10% mismatch",
-        all(!office_miss) & any(adiff > 0.10, na.rm = FALSE) ~ "red",
+        all(!office_miss) & all(adiff < 0.01) & all(adiff <  0.01) & any(diff != 0, na.rm = FALSE) ~ "any < 1% mismatch",
+        all(!office_miss) & all(adiff < 0.05) & any(adiff >= 0.01) ~ "any < 5% mismatch",
+        all(!office_miss) & all(adiff < 0.10) & any(adiff >= 0.05) ~ "any < 10% mismatch",
+        all(!office_miss) & any(adiff > 0.10) ~ "red",
         any(office_miss) & any(!office_miss) ~ "candidate missing",
         all(office_miss) ~ "not collected",
         .default = "unclassified"
