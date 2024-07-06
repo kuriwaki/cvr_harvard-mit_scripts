5d4953ed0bf4cdb6ff3ce111ef2196227c01dff9
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-07-06 19:26:25 -0400

    Unify Harvard write-in candidates to `candidate == party == WRITEIN" (instead of W-I or WRITE-IN, as pointed out in https://github.com/kuriwaki/cvr_harvard-mit_scripts/pull/326#discussion_r1667214413). These will then have a party_short of "W-I"

--- R/prepare/fmt_release.R
+++ R/prepare/fmt_release.R
@@ -2,12 +2,18 @@ fmt_for_release <- function(tbl) {
   tbl |>
     select(-matches("jurisdiction_")) |>
     mutate(
+      # harvard sometimes uses W-I for their writein candidate's party; change this to WRITEIN
+      party_detailed = replace(party_detailed, candidate == "WRITE-IN" | party_detailed == "W-I", "WRITEIN"),
+      # standardize harvard candidate field to match medsl
+      candidate = replace(candidate, candidate == "WRITE-IN", "WRITEIN")
+      ) |>
+    mutate(
       party = NA_character_,
       party = ifelse(party_detailed == "DEMOCRAT",    "DEM", party),
       party = ifelse(party_detailed == "REPUBLICAN",  "REP", party),
       party = ifelse(party_detailed == "LIBERTARIAN", "LBT", party),
       party = ifelse(party_detailed == "GREEN",       "GRN", party),
-      party = ifelse(party_detailed == "WRITE-IN",    "W-I", party),
+      party = ifelse(party_detailed == "WRITEIN",     "W-I", party),
       party = ifelse(!party %in% c("DEM", "REP", "LBT", "GRN", "W-I"), "OTH", party),
       .before = party_detailed
     )
