12de00a514e800f41ebd41611e5fd7bc854b3ecd
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-07-06 20:08:13 -0400

    Fixes duplicates for president by the other fringe candidate. Also change replace to ifelse for arrow

--- R/prepare/fmt_release.R
+++ R/prepare/fmt_release.R
@@ -2,14 +2,14 @@ fmt_for_release <- function(tbl) {
   tbl |>
     select(-matches("jurisdiction_")) |>
     # https://github.com/kuriwaki/cvr_harvard-mit_scripts/issues/308#issuecomment-2212053293
-    filter(!(candidate == "KANYE WEST" & is.na(party_detailed) & state == "UTAH" & county_name == "SAN JUAN")) |>
+    filter(!(candidate %in% c("KANYE WEST", "JOE MCHUGH ELIZABETH STORM") & is.na(party_detailed) & state == "UTAH" & county_name == "SAN JUAN")) |>
     mutate(
       # harvard sometimes uses W-I for their writein candidate's party; change this to WRITEIN
-      party_detailed = replace(party_detailed, candidate == "WRITE-IN" | party_detailed == "W-I", "WRITEIN"),
+      party_detailed = ifelse(candidate == "WRITE-IN" | party_detailed == "W-I", "WRITEIN", party_detailed),
       # standardize harvard candidate field to match medsl
-      candidate = replace(candidate, candidate == "WRITE-IN", "WRITEIN"),
+      candidate = ifelse(candidate == "WRITE-IN", "WRITEIN", candidate),
       # standardized MEDSL's writein's parties to simply WRITEIN as well
-      party_detailed = replace(party_detailed, candidate == "WRITEIN", "WRITEIN")
+      party_detailed = ifelse(candidate == "WRITEIN", "WRITEIN", party_detailed)
       ) |>
     mutate(
       party = NA_character_,
