56797b7308d87fcfeffb566830225eef96a3b859
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-07-08 10:52:53 -0400

    Temp fix for #308 Ohio Seneca

--- R/release/01_coalesce-teams.R
+++ R/release/01_coalesce-teams.R
@@ -38,10 +38,19 @@ ds_meds <- open_dataset(path(PATH_parq, "medsl/"))
 ds_harv <- open_dataset(path(PATH_parq, "harvard/"))
 ds_harv_sel <- ds_harv |> semi_join(hv_counties, by = c("state", "county_name"))
 
+# temp Fixes
+# https://github.com/kuriwaki/cvr_harvard-mit_scripts/issues/308#issuecomment-2212066285
+seneca_rm <- ds_meds |>
+  filter( state == "OHIO", county_name == "SENECA", office == "STATE HOUSE") |>
+  collect() |>
+  filter(n() == 2, .by = cvr_id) |>
+  filter(district == "087")
+
 ## remove from MEDSL
 ds_meds |>
   anti_join(rm_counties, by = c("state", "county_name")) |>
   anti_join(hv_counties, by = c("state", "county_name")) |>
+  anti_join(seneca_rm, by = c("state", "county_name", "cvr_id", "office", "district")) |>
   reallocate_wi_prec() |>
   fmt_for_release() |>
   left_join(prec_names, by = c("state", "county_name", "precinct"), relationship = "many-to-one") |>
