603731aa1cb862b8bbcce54e54c3a759d66b44c8
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-07-07 22:45:19 -0400

    Rewrite dedupe functin more comprehensively

--- R/release/03_check-release.R
+++ R/release/03_check-release.R
@@ -1,8 +1,8 @@
-library(tidyverse)
-library(arrow)
-library(fs)
-
-gc()
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(arrow)
+  library(fs)
+})
 
 username <- Sys.info()["user"]
 if (username %in% c("shirokuriwaki", "sk2983")) {
@@ -32,16 +32,33 @@ open_dataset(PATH_release) |>
   collect()
 
 # check for duplicates
-for (j in c("US PRESIDENT", "GOVERNOR", "US HOUSE", "US SENATE",
-            "STATE SENATE")) {
+for (j in c("US PRESIDENT", "GOVERNOR", "US HOUSE",
+            "US SENATE", "STATE SENATE", "STATE HOUSE")) {
 
-  dups_count <- open_dataset(PATH_release) |>
-    filter(office == j) |>
-    count(state, county_name, cvr_id, district) |>
+  ds_j <- open_dataset(PATH_release) |> filter(office == j)
+
+  if (j == "US SENATE") {
+    count_j <- bind_rows(
+      ds_j |> filter(state != "GEORGIA") |> count(state, county_name, cvr_id) |> collect(),
+      ds_j |> filter(state == "GEORGIA") |> count(state, county_name, cvr_id, district) |> collect()
+    )
+  }
+
+  if (j == "STATE HOUSE") {
+    count_j <- ds_j |>
+      filter(!state %in% c("WEST VIRGINIA", "ARIZONA")) |>
+      count(state, county_name, cvr_id)
+  }
+
+  if (j %in% c("US HOUSE", "STATE SENATE", "US PRESIDENT", "GOVERNOR")) {
+    count_j <- ds_j |> count(state, county_name, cvr_id)
+  }
+
+  dup_count <- count_j |>
     count(n, name = "nn") |>
     collect()
-  gc()
 
-  stopifnot(nrow(dups_count) == 1)
-  cat(j, "checked\n")
+  stopifnot(nrow(dup_count) == 1)
+  cli::cli_alert_success("{j} duplicate checked")
+  gc()
 }
