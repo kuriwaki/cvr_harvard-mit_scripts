73d4c46d57cb8ec71930afa269356ba2a417eeef
Mason Reece <mpreece@mit.edu>
2024-07-09 16:38:37 -0400

    mark build-harvard as unused

---
+++ code/[UNUSED] build-harvard/02_merge-party_snyder.R
@@ -0,0 +1,125 @@
+library(arrow)
+library(duckplyr)
+library(tidyverse)
+library(fs)
+
+# unlimited memory
+# https://duckdb.org/docs/guides/performance/how_to_tune_workloads.html
+# con <- duckplyr:::get_default_duckdb_connection()
+# DBI::dbExecute(con, "SET preserve_insertion_order = false;")
+
+Sys.setenv(DUCKPLYR_FORCE = TRUE)
+
+source("R/build-harvard/R/parse.R")
+
+# Datasets -- metadata----
+meta    <- duckplyr_df_from_parquet(path(PATH_projdir, "to-parquet", "item_choice_info/*.parquet"))
+ds_orig <- duckplyr_df_from_parquet(PATH_long2) |>
+  select(-line_number)
+ds_prec <- duckplyr_df_from_parquet(path(PATH_prec_js, "*/*.parquet"))
+
+# filenames
+filenames <- read_csv("R/build-harvard/metadata/input_files.txt",
+                      name_repair = "unique_quiet",
+                      show_col_types = FALSE)$file
+
+# Main merge ----
+
+tictoc::tic()
+walk(
+  .x = rev(filenames),
+  .f = function(x, dir = PATH_projdir) {
+    # get state and county
+    st <- as.character(parse_js_fname(x)["state"])
+    ct <- as.character(parse_js_fname(x)["county"])
+    cli::cli_progress_bar(format = "State {st}")
+
+    ds <- ds_orig |>
+      filter(state == st, county == ct) |>
+      inner_join(
+        select(meta,
+               state, county,
+               office_type,
+               column, item, choice_id,
+               dist,
+               party,
+               incumbent,
+               measure,
+               place,
+               topic,
+               unexp_term,
+               num_votes),
+        by = c("state", "county", "column", "item", "choice_id"),
+        relationship = "many-to-one")
+
+    # Precinct
+    ds_prec_st <- ds_prec |>
+      filter(state == st, county == ct)
+
+    # Party ID ---
+    # follows Snyder "MAKE PARTISANSHIP" in `analysis_all_politics_partisan.do`
+    ds_pid <- ds |>
+      filter(office_type == "federal") |>
+      mutate(
+        D = as.integer(party == "DEM"),
+        R = as.integer(party == "REP"),
+      ) |>
+      summarize(
+        pres = if_else(
+          any(D == 1 & item == "US_PRES"),  "D",
+          if_else(
+            any(R == 1 & item == "US_PRES"), "R",
+            if_else(
+              any(party == "LBT" & item == "US_PRES"), "LBT",
+              NA_character_
+            )
+          )
+        ),
+        pid = if_else(
+          sum(D)  > 0 & sum(R) == 0, "DEM",
+          if_else(
+            sum(D)  > 0 & sum(R) > 0, "SPL",
+            if_else(
+              sum(D) == 0 & sum(R) > 0, "REP",
+              NA_character_
+            )
+          )
+        ),
+        .by = c(state, county, cvr_id)
+      ) |>
+      select(state, county, cvr_id, pres, pid)
+
+    # Write ----
+    ds |>
+      # merge prec
+      left_join(
+        ds_prec_st,
+        by = c("state", "county", "cvr_id"),
+        relationship = "many-to-one") |>
+      # merge national party
+      left_join(
+        ds_pid,
+        by = c("state", "county", "cvr_id"),
+        relationship = "one-to-one") |>
+      relocate(precinct, pres, pid, .after = cvr_id) |>
+      write_dataset(
+        path = PATH_merged,
+        format = "parquet",
+        partitioning = c("state", "county"),
+        existing_data_behavior = "delete_matching")
+  },
+  .progress = "counties"
+)
+tictoc::toc()
+# 80min
+
+
+# to Dropbox
+upload_to_db <- FALSE
+if (upload_to_db) {
+  fs::dir_copy(
+    PATH_merged,
+    "~/Dropbox/CVR_Data_Shared/data_main/parquet",
+    overwrite = TRUE
+  )
+}
---
+++ code/[UNUSED] build-harvard/fmt_to_medsl.R
@@ -0,0 +1,81 @@
+
+#' Format to match MEDSL's variable names
+#'
+#'
+fmt_harv_to_medsl <- function(tbl) {
+
+  atlarge <- c("ALASKA", "DELAWARE", "WYOMING", "VERMONT", "MONTANA",
+               "NORTH DAKOTA", "SOUTH DAKOTA")
+
+
+  tbl |>
+    mutate(
+      county_name = str_to_upper(str_replace_all(county, "_", " ")),
+      office = case_match(item,
+                          "US_PRES" ~ "US PRESIDENT",
+                          "US_SEN" ~ "US SENATE",
+                          "US_SEN (S)" ~ "US SENATE",
+                          "US_REP" ~ "US HOUSE",
+                          "ST_SEN" ~ "STATE SENATE",
+                          "ST_REP" ~ "STATE HOUSE",
+                          "ST_GOV" ~ "GOVERNOR",
+      ),
+      candidate = str_to_upper(choice),
+      state = case_match(state,
+                         "AZ" ~ "ARIZONA",
+                         "AK" ~ "ALASKA",
+                         "CA" ~ "CALIFORNIA",
+                         "CO" ~ "COLORADO",
+                         "DE" ~ "DELAWARE",
+                         "FL" ~ "FLORIDA",
+                         "GA" ~ "GEORGIA",
+                         "IL" ~ "ILLINOIS",
+                         "MD" ~ "MARYLAND",
+                         "MI" ~ "MICHIGAN",
+                         "NJ" ~ "NEW JERSEY",
+                         "NM" ~ "NEW MEXICO",
+                         "NV" ~ "NEVADA",
+                         "OH" ~ "OHIO",
+                         "OR" ~ "OREGON",
+                         "RI" ~ "RHODE ISLAND",
+                         "TN" ~ "TENNESSEE",
+                         "TX" ~ "TEXAS",
+                         "WV" ~ "WEST VIRGINIA",
+                         "WI" ~ "WISCONSIN",
+                         "UT" ~ "UTAH"
+      ),
+      party_detailed = case_match(party,
+                                  "DEM" ~ "DEMOCRAT",
+                                  "DEM?" ~ "DEMOCRAT",
+                                  "REP" ~ "REPUBLICAN",
+                                  "REP?" ~ "REPUBLICAN",
+                                  "LBT" ~ "LIBERTARIAN",
+                                  "IND" ~ "INDEPENDENT",
+                                  "GRN" ~ "GREEN",
+                                  "GRE" ~ "GREEN",
+                                  "NPA" ~ "NO PARTY AFFILIATION",
+                                  .default = party
+      ),
+      party_detailed = replace(party_detailed, choice %in% c("UNDERVOTE", "UNDERVOTE?"), "undervote"),
+      party_detailed = replace(party_detailed, choice != "NA" & party_detailed == "LIB", "LIBERTARIAN"),
+    ) |>
+    # district formatting for MEDSL
+    mutate(district = str_pad(dist, width = 3, pad = "0"),
+           district = replace(district, office == "US PRESIDENT", "FEDERAL"),
+           district = replace(district, office == "US HOUSE" & state %in% atlarge, "000")) |>
+    mutate(dist_state = replace(state, !office %in% c("GOVERNOR", "US SENATE"), NA),
+           district = coalesce(dist_state, district),
+           district = replace(district, item == "US_SEN (S)" & state == "GEORGIA", "GEORGIA-III")) |> # class III senate seat
+    select(state,
+           county_name,
+           matches("cvr_id"),
+           matches("precinct"),
+           matches("pres"),
+           office,
+           district,
+           candidate,
+           matches("column"),
+           magnitude = num_votes,
+           party,
+           party_detailed)
+}
