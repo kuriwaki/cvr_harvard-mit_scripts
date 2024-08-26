f7311b2f81b6e35b068a1cb11d8b274a4e7be459
taran samarth <5815717+taransamarth@users.noreply.github.com>
2024-08-26 17:42:14 -0400

    Add excel sheet for county list table

--- code/build-analysis/02b_list-counties.R
+++ code/build-analysis/02b_list-counties.R
@@ -2,29 +2,31 @@ library(tidyverse)
 library(arrow)
 library(glue)
 library(readxl)
+library(openxlsx)
 
 # count voters by county
-dat <- open_dataset("release") |> 
-  filter(office == "US PRESIDENT") |> 
-  count(state, county_name, name = "voters_pres") |> 
-  collect() |> 
+dat <- open_dataset("release") |>
+  filter(office == "US PRESIDENT") |>
+  count(state, county_name, name = "voters_pres") |>
+  collect() |>
   mutate(county_name = replace(
-    county_name, 
+    county_name,
     state %in% c("RHODE ISLAND", "DELAWARE", "DISTRICT OF COLUMBIA"),
     "STATEWIDE"))
 
 # Subset to releasable counties, reformat county name
-dat_fmt <- dat |> 
+dat_fmt <- dat |>
   mutate(
     n_counties = n(),
     n_voters = scales::comma(sum(voters_pres)),
     .by = state
-  ) |> 
+  ) |>
   mutate(state = str_to_title(state),
          county = str_to_title(county_name),
-         county = recode(county, Statewide = "(Statewide)")) |> 
+         county = recode(county, Statewide = "(Statewide)")) |>
   arrange(state, county)
 
+rows_list <- list()
 
 # line by line so we can do multicols ----
 st_counter <- ""
@@ -35,20 +37,34 @@ for (i in 1:nrow(dat_fmt)) {
   # new state
   if (dat_fmt$state[i] != st_counter) {
     st <- dat_fmt$state[i]
-    
-    if (st != dat_fmt$state[1]) 
+
+    if (st != dat_fmt$state[1]) {
       cat("\\\\", "\n")
-    
+      rows_list <- append(rows_list, list(c("")))
+    }
+
     cat(glue("\\textbf{[st]}\\\\", .open = "[", .close = "]"), "\n")
+    rows_list <- append(rows_list, st)
     cat("{\\raggedright \\itshape ")
-    if (!st %in% c("Alaska", "Delaware", "District of Columbia", "Rhode Island"))
-      cat(cli::pluralize("{dat_fmt$n_counties[i]} count{?y/ies}, "))
-    
-    cat(glue("{dat_fmt$n_voters[i]} voters \\par}"), "\n")
+    if (!st %in% c("Alaska", "Delaware", "District of Columbia", "Rhode Island")) {
+      cat(cli::pluralize("{dat_fmt$n_counties[i]} count{?y/ies}, {dat_fmt$n_voters[i]} voters \\par}"), "\n")
+      rows_list <- append(rows_list, cli::pluralize("{dat_fmt$n_counties[i]} count{?y/ies}, {dat_fmt$n_voters[i]} voters"))
+    } else {
+      cat(glue("{dat_fmt$n_voters[i]} voters \\par}"), "\n")
+      rows_list <- append(rows_list, glue("{dat_fmt$n_voters[i]} voters}"))
+    }
+
     st_counter <- st
   }
-  
+
   # print county
   cat(dat_fmt$county[i], "\\\\\n")
+  rows_list <- append(rows_list, list(dat_fmt$county[i]))
 }
+
 sink()
+
+output_df <- data.frame(data = unlist(rows_list), stringsAsFactors = FALSE)
+
+write.xlsx(output_df, "tables/tab_counties_text.xlsx", rowNames = FALSE)
+
