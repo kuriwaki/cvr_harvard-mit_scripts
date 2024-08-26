030e3b738c92c2d72c6bafa5c706cef41ce5580a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-08-26 19:18:28 -0400

    Undo rename, I now see the reason

--- code/04_build-release/build-release.sh
+++ code/04_build-release/build-release.sh
@@ -1,4 +1,3 @@
 Rscript code/01_coalesce-teams.R
-Rscript code/02_compare.R
-Rscript code/03_subset-to-release.R
-Rscript code/04_check-release.R
+Rscript code/02_subset-to-release.R
+Rscript code/03_check-release.R
