9bd09009719820c5128ad30a2e2140ce1e158c7e
Mason Reece <mpreece@mit.edu>
2024-08-26 15:16:31 -0400

    remove 05_paper-analyze

--- code/05_paper-analyze/paper.sh
+++
@@ -1,8 +0,0 @@
-Rscript 01_numbers.R
-Rscript 02a_map-states.R
-Rscript 02b_list-counties.R
-Rscript 03a_state-tilt.R
-Rscript 03b_county-tilt.R
-Rscript 04_errors.R
-Rscript 05_precinct-errors.R
-quarto render usage/usage.qmd
